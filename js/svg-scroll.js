1efc230f026f8aa220f3d0e93c1dc87adca7afcc
vincent_parquet <vincentparquet@hotmail.fr>
2015-07-16 18:44:33 +0200

    add files

---
+++ js/svg-scroll.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  
+  //set the units for the svg paths
+  $(".path").css("stroke-dashoffset", "1000");
+  $(".path").css("stroke-dasharray", "1000");
+
+  //variable for the 'stroke-dashoffset' unit
+  var $dashOffset = $(".path").css("stroke-dashoffset");//.star
+
+  //on a scroll event - execute function
+  $(window).scroll(function () {    
+
+    //calculate how far down the page the user is 
+    var $percentageComplete = (($(window).scrollTop()/($("html").height()-$(window).height()))*100);    
+
+    //convert dashoffset pixel value to interger
+    var $newUnit = parseInt($dashOffset, 10); 
+
+    //convert percentage complete unit to an interger
+//    var $parsedPercentage = parseInt($percentageComplete, 10);
+
+    //get the value to be subtracted from the 'stroke-dashoffset'
+    var $offsetUnit = $percentageComplete * ($newUnit / 100);
+
+    //set the new value of the dashoffset to create the drawing effect
+    $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);//.star
+
+  });
+
+});
+
+/*
+Copyright (c) 2015 by Captain Anonymous (http://codepen.io/anon/pen/Nqzerr)
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
