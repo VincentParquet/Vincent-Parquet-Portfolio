2af581a6682fa8a3343e6e1faa3b56adf1e02341
vincent_parquet <vincentparquet@hotmail.fr>
2015-07-22 11:37:10 +0200

    add files

--- js/svg-scroll.js
+++ js/svg-scroll.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
   //set the units for the svg paths
   $(".path").css("stroke-dashoffset", "1000");
   $(".path").css("stroke-dasharray", "1000");
-
+  
+  //add invisible text
+  //$(".tex").css("visibility","hidden");
+  
   //variable for the 'stroke-dashoffset' unit
   var $dashOffset = $(".path").css("stroke-dashoffset");//.star
 
@@ -21,7 +24,21 @@ $(document).ready(function() {
 
     //set the new value of the dashoffset to create the drawing effect
     $(".path").css("stroke-dashoffset", $newUnit - $offsetUnit);//.star
-
+    
+    //if(($(window).scrollTop())>((document.getElementById("rect").getBBox().y)+70)){ // $(".rect").points.getItem().y)
+    //    $(".tex").css("visibility","visible");
+    //}
   });
 
 });
+
+
+
+function getSmallImg(k) {
+            document.getElementById('blanc1').style.visibility  = "visible";
+            document.getElementById('principale').src = k;
+            document.getElementById('mapage').style.background = "url(img/s0_bback3.png),url(img/se5_page_30.jpg) no-repeat center fixed";
+            document.getElementById('mapage').style.backgroundSize = "10px 8px, cover";
+          }
+    
+          p1 = "img/se5_page_1.jpg";
